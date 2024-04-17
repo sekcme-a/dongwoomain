537f9987de3fa2e609d2b0385a17b73edc2a96aa
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2024-04-17 15:08:54 +0900

    asdf

--- pages/_app.js
+++ pages/_app.js
@@ -44,7 +44,6 @@ const MyApp = ({Component, pageProps}) => {
                 <Head>
                     <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
                     <meta charSet="utf-8" />
-                    <link rel="canonical" href="https://xn--2i0bm5iryeh7q.kr/"></link>
                     <meta name="robots" content="index,follow"></meta>
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                 </Head>
